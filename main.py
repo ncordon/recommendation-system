dc37e0e4d53ee31ec5a07b5d206648406867cf33
Lothar Soto Palma <lsp.lot94@gmail.com>
2017-03-06 16:02:54 +0100

    Test deploy 2

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Dani es fantástico.')
+        self.response.write('Dani es fantastico.')
 
 
 app = webapp2.WSGIApplication([
